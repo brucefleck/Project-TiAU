48623062ac614e94d7fe4936c703bfcf699ddf35
Bruce <brucematiasfleck@gmail.com>
2023-11-21 23:34:57 -0500

    some more updates

--- php/process_login.php
+++ php/process_login.php
@@ -1,22 +1,11 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $name = $_POST['name'];
     $email = $_POST['email'];
+    $password = $_POST['password'];
 
     // Use the retrieved data as needed, e.g., insert into a database, perform calculations, etc.
     // For example, echoing the data back:
-    echo "Name: $name <br>";
-    echo "Email: $email <br>";
+    echo "Email: $name <br>";
+    echo "Password: $password <br>";
 }
 ?>
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Success Page</title>
-</head>
-<body>
-    <h1>Success!</h1>
-    <p>Name: $name</p>
-    <p>Email: $email</p>
-</body>
-</html>
