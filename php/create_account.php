9fe95a9b80402d714e63db9ab9fc6b7ea71da1de
Bruce <brucematiasfleck@gmail.com>
2023-11-22 06:54:03 -0500

    asdasd

--- php/create_account.php
+++ php/create_account.php
@@ -2,7 +2,6 @@
 <html>
 <head>
     <title>Cuenta Creada - TiAU</title>
-    <link rel="stylesheet" href="..\css\acc-created-stylesheet.css">
     <link rel="stylesheet" href="..\css\universal-stylesheet.css">
     <link rel="icon" href="..\assets\images\upc-logo-red.webp" type="image/webp">
 </head>
@@ -41,34 +40,39 @@
         //echo "<p>Email: $email</p>";
         //echo "<p>Password: $password</p>";
 
-        echo '<div class="acc-created-container">
-        <div class="acc-created-msg">
-            <h2>Bienvenido, '.$name.'!!!</h2>
-            <p>Ya esta registrada tu cuenta :D</p>
-        </div>
-        <div class="goto-login-button">
-            <a href="index.html">
-                <button style="background-color: #2A29B3;
-                color: whitesmoke;
-                padding: 1rem 1.5rem;
-                border: none;
-                text-align: center;
-                text-decoration: none;
-                display: inline-block;
-                border-radius: 4px;" class="primary-button goto-login">Ir a login!</button>
-            </a>
-        </div>
-        <div class="decoration">
-                <div class="img-container">
-                    <img src="..\assets\images\tiau-logo_svg.svg" alt="tiau-logo">
-                </div>
-                <div class="colors-container">
-                    <div class="yellow"></div>
-                    <div class="orange"></div>
-                    <div class="pink"></div>
-                </div>
+        echo '<div style="margin-left: auto; margin-right: auto; margin-top: 8rem;
+        border-radius: 4px; width: 20rem; border-radius: 10px;
+        box-shadow: 15px 15px 25px 20px rgb(220, 218, 218);
+        padding: 2rem; justify-content: center;">
+            <div style="width: 17rem; justify-content: center; margin-left: auto; margin-right: auto;">
+                <h2>Bienvenido, '.$name.'!!!</h2>
+                <p>Ya esta registrada tu cuenta :D</p>
             </div>
-        </div>';
+            <div style="width: 7rem; margin-left: auto; margin-right: auto;">
+                <a href="index.html">
+                    <button style="background-color: #2A29B3;
+                    color: whitesmoke;
+                    padding: 1rem 1.5rem;
+                    border: none;
+                    text-align: center;
+                    text-decoration: none;
+                    display: inline-block;
+                    border-radius: 4px;" class="primary-button goto-login">Ir a login!</button>
+                </a>
+            </div>
+            <div style="display: flex; flex-direction: row; width: 17rem; margin-top: 1rem; margin-left: auto; margin-right: auto;">
+                    <div style="background-color: #2A29B3;
+                    display: flex;
+                    justify-content: flex-end; height: 7rem;">
+                        <img style="height: 7rem; width: 12rem;" src="..\assets\images\tiau-logo_svg.svg" alt="tiau-logo">
+                    </div>
+                    <div class="colors-container">
+                        <div style="width: 5rem; height: 2.33rem; background-color: #FFB100;"></div>
+                        <div style="width: 100%; height: 2.33rem; background-color: #FF4B00;"></div>
+                        <div style="width: 100%; height: 2.33rem; background-color: #FE6EB4"></div>
+                    </div>
+                </div>
+            </div>';
 
         $sql = "INSERT INTO users (name, email, password) VALUES ('$name', '$email', '$password')";
 
