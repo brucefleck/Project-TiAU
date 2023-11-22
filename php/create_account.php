f27fd4639b1edb7d8419230f62a2e49d2bc2c1e1
Bruce <brucematiasfleck@gmail.com>
2023-11-22 06:02:29 -0500

    asdasd

--- php/create_account.php
+++ php/create_account.php
@@ -16,32 +16,34 @@
         die("Connection failed: " . $conn->connect_error);
     }
     
-    //CHECK IF EMAIL IS ALREADY REGISTERED
-    $email = $_POST['email'];
-    echo "<h3>email: $email</h3>";
+    ////CHECK IF EMAIL IS ALREADY REGISTERED
+    //$email = $_POST['email'];
+    //echo "<h3>email: $email</h3>";
+    //
+    //$check_query = "SELECT * FROM users WHERE email = '$email'";
+    //$result = $connection->query($check_query);
+    //
+    //if ($result->num_rows > 0) {
+    //    echo "<script>alert('Email already exists. Please use a different email.');</script>";
+    //} else {
+    //    
+    //}
 
-    $check_query = "SELECT * FROM users WHERE email = '$email'";
-    $result = $connection->query($check_query);
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $name = $_POST['first_name'];
+        $email = $_POST['email'];
+        $password = $_POST['password1'];
 
-    if ($result->num_rows > 0) {
-        echo "<script>alert('Email already exists. Please use a different email.');</script>";
-    } else {
-        if ($_SERVER["REQUEST_METHOD"] == "POST") {
-            $name = $_POST['first_name'];
-            $email = $_POST['email'];
-            $password = $_POST['password1'];
-    
-            echo "<p>Nombre: $name</p>";
-            echo "<p>Email: $email</p>";
-            echo "<p>Password: $password</p>";
-    
-            $sql = "INSERT INTO users (name, email, password) VALUES ('$name', '$email', '$password')";
-    
-            if ($conn->query($sql) === TRUE) {
-                echo "New record created successfully";
-            } else {
-                echo "Error: " . $sql . "<br>" . $conn->error;
-            }
+        echo "<p>Nombre: $name</p>";
+        echo "<p>Email: $email</p>";
+        echo "<p>Password: $password</p>";
+
+        $sql = "INSERT INTO users (name, email, password) VALUES ('$name', '$email', '$password')";
+
+        if ($conn->query($sql) === TRUE) {
+            echo "New record created successfully";
+        } else {
+            echo "Error: " . $sql . "<br>" . $conn->error;
         }
     }
 
