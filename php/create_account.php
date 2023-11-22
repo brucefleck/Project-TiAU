1b62e93466d1e7a2c455e8e70397c340a9e635ed
Bruce <brucematiasfleck@gmail.com>
2023-11-22 02:05:13 -0500

    asdasdasd#

--- php/create_account.php
+++ php/create_account.php
@@ -1,33 +1,29 @@
-<?php
-$servername = "2a02:4780:13:1174:0:f12:1917:2"; // Replace with your server name
-$username = "u252844311_brucefleck"; // Replace with your database username
-$password = "1JVp3K$N78DEPq4Z$wnn"; // Replace with your database password
-$dbname = "u252844311_TIAU_DB"; // Replace with your database name
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Database Connection Test</title>
+</head>
+<body>
+    <?php
+    // Database connection details
+    $servername = "2a02:4780:13:1174:0:f12:1917:2"; // Replace with your server name
+    $username = "u252844311_brucefleck"; // Replace with your database username
+    $password = "1JVp3K$N78DEPq4Z$wnn"; // Replace with your database password
+    $dbname = "u252844311_TIAU_DB"; // Replace with your database name
 
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
+    // Attempt to create a connection
+    $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-
-// Process form data
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $name = $_POST['first_name'];
-    $email = $_POST['email'];
-    $password = $_POST['password1'];
-
-    // SQL query to insert data into the database
-    $sql = "INSERT INTO users (name, email, password) VALUES ('$name', '$email', '$password')";
-
-    if ($conn->query($sql) === TRUE) {
-        echo "New record created successfully";
+    // Check connection
+    if ($conn->connect_error) {
+        echo "<h2>Connection Failed</h2>";
+        echo "Error: " . $conn->connect_error;
     } else {
-        echo "Error: " . $sql . "<br>" . $conn->error;
+        echo "<h2>Connection Successful</h2>";
     }
-}
 
-// Close the connection
-$conn->close();
-?>
+    // Close the connection
+    $conn->close();
+    ?>
+</body>
+</html>
