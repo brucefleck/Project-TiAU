6c1eae6260d704b62f9ca2fd976d72184d05c8d9
Bruce <brucematiasfleck@gmail.com>
2023-11-22 07:12:41 -0500

    asd

--- php/process_login.php
+++ php/process_login.php
@@ -1,41 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Homepage</title>
+</head>
+<body>
 <?php
-session_start();
+    session_start();
 
-// Database connection
-$servername = "localhost";
-$username = "u252844311_brucefleck";
-$password = "Moocat12#";
-$dbname = "u252844311_TIAU";
+    // Database connection
+    $servername = "localhost";
+    $username = "u252844311_brucefleck";
+    $password = "Moocat12#";
+    $dbname = "u252844311_TIAU";
 
-$conn = new mysqli($servername, $username, $password, $dbname);
+    $conn = new mysqli($servername, $username, $password, $dbname);
 
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-
-$email = $_POST['email'];
-$password = $_POST['password'];
-
-$query = "SELECT * FROM users WHERE email = ?";
-$stmt = $connection->prepare($query);
-$stmt->bind_param("s", $email);
-$stmt->execute();
-$result = $stmt->get_result();
-
-if ($result->num_rows > 0) {
-    $user = $result->fetch_assoc();
-    if (password_verify($password, $user['password'])) {
-        $_SESSION['user_id'] = $user['id'];
-        echo "Login successful! Redirecting...";
-        header("Location: dashboard.php");
-        exit();
-    } else {
-        echo "Incorrect password";
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
     }
-} else {
-    echo "User not found";
-}
 
-$stmt->close();
-$connection->close();
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+
+    $query = "SELECT * FROM users WHERE email = ?";
+    $stmt = $connection->prepare($query);
+    $stmt->bind_param("s", $email);
+    $stmt->execute();
+    $result = $stmt->get_result();
+
+    if ($result->num_rows > 0) {
+        $user = $result->fetch_assoc();
+        if (password_verify($password, $user['password'])) {
+            $_SESSION['user_id'] = $user['id'];
+            echo "Login successful! Redirecting...";
+            header("Location: dashboard.php");
+            exit();
+        } else {
+            echo "Incorrect password";
+        }
+    } else {
+        echo "User not found";
+    }
+
+    $stmt->close();
+    $connection->close();
 ?>
+</body>
+</html>
